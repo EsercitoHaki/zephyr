ddc21522c6838f178383f8018301596b534d1a5f
EsercitoHaki <esercitohaki2003@gmail.com>
2025-03-26 16:03:58 +0700

    first commit

---
+++ build/engine/libs/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake
@@ -0,0 +1,56 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/esercito/Workspaces/Vulkan/zephyr/engine/libs/glfw/src/context.c" "engine/libs/glfw/src/CMakeFiles/glfw.dir/context.c.o" "gcc" "engine/libs/glfw/src/CMakeFiles/glfw.dir/context.c.o.d"
+  "/home/esercito/Workspaces/Vulkan/zephyr/engine/libs/glfw/src/egl_context.c" "engine/libs/glfw/src/CMakeFiles/glfw.dir/egl_context.c.o" "gcc" "engine/libs/glfw/src/CMakeFiles/glfw.dir/egl_context.c.o.d"
+  "/home/esercito/Workspaces/Vulkan/zephyr/engine/libs/glfw/src/glx_context.c" "engine/libs/glfw/src/CMakeFiles/glfw.dir/glx_context.c.o" "gcc" "engine/libs/glfw/src/CMakeFiles/glfw.dir/glx_context.c.o.d"
+  "/home/esercito/Workspaces/Vulkan/zephyr/engine/libs/glfw/src/init.c" "engine/libs/glfw/src/CMakeFiles/glfw.dir/init.c.o" "gcc" "engine/libs/glfw/src/CMakeFiles/glfw.dir/init.c.o.d"
+  "/home/esercito/Workspaces/Vulkan/zephyr/engine/libs/glfw/src/input.c" "engine/libs/glfw/src/CMakeFiles/glfw.dir/input.c.o" "gcc" "engine/libs/glfw/src/CMakeFiles/glfw.dir/input.c.o.d"
+  "/home/esercito/Workspaces/Vulkan/zephyr/engine/libs/glfw/src/linux_joystick.c" "engine/libs/glfw/src/CMakeFiles/glfw.dir/linux_joystick.c.o" "gcc" "engine/libs/glfw/src/CMakeFiles/glfw.dir/linux_joystick.c.o.d"
+  "/home/esercito/Workspaces/Vulkan/zephyr/engine/libs/glfw/src/monitor.c" "engine/libs/glfw/src/CMakeFiles/glfw.dir/monitor.c.o" "gcc" "engine/libs/glfw/src/CMakeFiles/glfw.dir/monitor.c.o.d"
+  "/home/esercito/Workspaces/Vulkan/zephyr/engine/libs/glfw/src/null_init.c" "engine/libs/glfw/src/CMakeFiles/glfw.dir/null_init.c.o" "gcc" "engine/libs/glfw/src/CMakeFiles/glfw.dir/null_init.c.o.d"
+  "/home/esercito/Workspaces/Vulkan/zephyr/engine/libs/glfw/src/null_joystick.c" "engine/libs/glfw/src/CMakeFiles/glfw.dir/null_joystick.c.o" "gcc" "engine/libs/glfw/src/CMakeFiles/glfw.dir/null_joystick.c.o.d"
+  "/home/esercito/Workspaces/Vulkan/zephyr/engine/libs/glfw/src/null_monitor.c" "engine/libs/glfw/src/CMakeFiles/glfw.dir/null_monitor.c.o" "gcc" "engine/libs/glfw/src/CMakeFiles/glfw.dir/null_monitor.c.o.d"
+  "/home/esercito/Workspaces/Vulkan/zephyr/engine/libs/glfw/src/null_window.c" "engine/libs/glfw/src/CMakeFiles/glfw.dir/null_window.c.o" "gcc" "engine/libs/glfw/src/CMakeFiles/glfw.dir/null_window.c.o.d"
+  "/home/esercito/Workspaces/Vulkan/zephyr/engine/libs/glfw/src/osmesa_context.c" "engine/libs/glfw/src/CMakeFiles/glfw.dir/osmesa_context.c.o" "gcc" "engine/libs/glfw/src/CMakeFiles/glfw.dir/osmesa_context.c.o.d"
+  "/home/esercito/Workspaces/Vulkan/zephyr/engine/libs/glfw/src/platform.c" "engine/libs/glfw/src/CMakeFiles/glfw.dir/platform.c.o" "gcc" "engine/libs/glfw/src/CMakeFiles/glfw.dir/platform.c.o.d"
+  "/home/esercito/Workspaces/Vulkan/zephyr/engine/libs/glfw/src/posix_module.c" "engine/libs/glfw/src/CMakeFiles/glfw.dir/posix_module.c.o" "gcc" "engine/libs/glfw/src/CMakeFiles/glfw.dir/posix_module.c.o.d"
+  "/home/esercito/Workspaces/Vulkan/zephyr/engine/libs/glfw/src/posix_poll.c" "engine/libs/glfw/src/CMakeFiles/glfw.dir/posix_poll.c.o" "gcc" "engine/libs/glfw/src/CMakeFiles/glfw.dir/posix_poll.c.o.d"
+  "/home/esercito/Workspaces/Vulkan/zephyr/engine/libs/glfw/src/posix_thread.c" "engine/libs/glfw/src/CMakeFiles/glfw.dir/posix_thread.c.o" "gcc" "engine/libs/glfw/src/CMakeFiles/glfw.dir/posix_thread.c.o.d"
+  "/home/esercito/Workspaces/Vulkan/zephyr/engine/libs/glfw/src/posix_time.c" "engine/libs/glfw/src/CMakeFiles/glfw.dir/posix_time.c.o" "gcc" "engine/libs/glfw/src/CMakeFiles/glfw.dir/posix_time.c.o.d"
+  "/home/esercito/Workspaces/Vulkan/zephyr/engine/libs/glfw/src/vulkan.c" "engine/libs/glfw/src/CMakeFiles/glfw.dir/vulkan.c.o" "gcc" "engine/libs/glfw/src/CMakeFiles/glfw.dir/vulkan.c.o.d"
+  "/home/esercito/Workspaces/Vulkan/zephyr/engine/libs/glfw/src/window.c" "engine/libs/glfw/src/CMakeFiles/glfw.dir/window.c.o" "gcc" "engine/libs/glfw/src/CMakeFiles/glfw.dir/window.c.o.d"
+  "/home/esercito/Workspaces/Vulkan/zephyr/engine/libs/glfw/src/wl_init.c" "engine/libs/glfw/src/CMakeFiles/glfw.dir/wl_init.c.o" "gcc" "engine/libs/glfw/src/CMakeFiles/glfw.dir/wl_init.c.o.d"
+  "/home/esercito/Workspaces/Vulkan/zephyr/engine/libs/glfw/src/wl_monitor.c" "engine/libs/glfw/src/CMakeFiles/glfw.dir/wl_monitor.c.o" "gcc" "engine/libs/glfw/src/CMakeFiles/glfw.dir/wl_monitor.c.o.d"
+  "/home/esercito/Workspaces/Vulkan/zephyr/engine/libs/glfw/src/wl_window.c" "engine/libs/glfw/src/CMakeFiles/glfw.dir/wl_window.c.o" "gcc" "engine/libs/glfw/src/CMakeFiles/glfw.dir/wl_window.c.o.d"
+  "/home/esercito/Workspaces/Vulkan/zephyr/engine/libs/glfw/src/x11_init.c" "engine/libs/glfw/src/CMakeFiles/glfw.dir/x11_init.c.o" "gcc" "engine/libs/glfw/src/CMakeFiles/glfw.dir/x11_init.c.o.d"
+  "/home/esercito/Workspaces/Vulkan/zephyr/engine/libs/glfw/src/x11_monitor.c" "engine/libs/glfw/src/CMakeFiles/glfw.dir/x11_monitor.c.o" "gcc" "engine/libs/glfw/src/CMakeFiles/glfw.dir/x11_monitor.c.o.d"
+  "/home/esercito/Workspaces/Vulkan/zephyr/engine/libs/glfw/src/x11_window.c" "engine/libs/glfw/src/CMakeFiles/glfw.dir/x11_window.c.o" "gcc" "engine/libs/glfw/src/CMakeFiles/glfw.dir/x11_window.c.o.d"
+  "/home/esercito/Workspaces/Vulkan/zephyr/engine/libs/glfw/src/xkb_unicode.c" "engine/libs/glfw/src/CMakeFiles/glfw.dir/xkb_unicode.c.o" "gcc" "engine/libs/glfw/src/CMakeFiles/glfw.dir/xkb_unicode.c.o.d"
+  "" "engine/libs/glfw/src/libglfw.so" "gcc" "engine/libs/glfw/src/CMakeFiles/glfw.dir/link.d"
+  )
+
+# Pairs of files generated by the same build rule.
+set(CMAKE_MULTIPLE_OUTPUT_PAIRS
+  "/home/esercito/Workspaces/Vulkan/zephyr/build/engine/libs/glfw/src/libglfw.so" "/home/esercito/Workspaces/Vulkan/zephyr/build/engine/libs/glfw/src/libglfw.so.3.5"
+  "/home/esercito/Workspaces/Vulkan/zephyr/build/engine/libs/glfw/src/libglfw.so.3" "/home/esercito/Workspaces/Vulkan/zephyr/build/engine/libs/glfw/src/libglfw.so.3.5"
+  )
+
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/engine/libs/glfw/src/CMakeFiles/glfw.dir/link.d
@@ -0,0 +1,166 @@
+libglfw.so.3.5: \
+  /usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib64/crti.o \
+  /usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/14.2.1/crtbeginS.o \
+  CMakeFiles/glfw.dir/context.c.o \
+  CMakeFiles/glfw.dir/init.c.o \
+  CMakeFiles/glfw.dir/input.c.o \
+  CMakeFiles/glfw.dir/monitor.c.o \
+  CMakeFiles/glfw.dir/platform.c.o \
+  CMakeFiles/glfw.dir/vulkan.c.o \
+  CMakeFiles/glfw.dir/window.c.o \
+  CMakeFiles/glfw.dir/egl_context.c.o \
+  CMakeFiles/glfw.dir/osmesa_context.c.o \
+  CMakeFiles/glfw.dir/null_init.c.o \
+  CMakeFiles/glfw.dir/null_monitor.c.o \
+  CMakeFiles/glfw.dir/null_window.c.o \
+  CMakeFiles/glfw.dir/null_joystick.c.o \
+  CMakeFiles/glfw.dir/posix_module.c.o \
+  CMakeFiles/glfw.dir/posix_time.c.o \
+  CMakeFiles/glfw.dir/posix_thread.c.o \
+  CMakeFiles/glfw.dir/x11_init.c.o \
+  CMakeFiles/glfw.dir/x11_monitor.c.o \
+  CMakeFiles/glfw.dir/x11_window.c.o \
+  CMakeFiles/glfw.dir/xkb_unicode.c.o \
+  CMakeFiles/glfw.dir/glx_context.c.o \
+  CMakeFiles/glfw.dir/wl_init.c.o \
+  CMakeFiles/glfw.dir/wl_monitor.c.o \
+  CMakeFiles/glfw.dir/wl_window.c.o \
+  CMakeFiles/glfw.dir/linux_joystick.c.o \
+  CMakeFiles/glfw.dir/posix_poll.c.o \
+  /usr/lib/librt.a \
+  /usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib64/libm.so \
+  /usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib64/libm.so \
+  /usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib64/libm.so \
+  /usr/lib/libm.so.6 \
+  /usr/lib/libmvec.so.1 \
+  /usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib64/libdl.a \
+  /usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/14.2.1/libgcc.a \
+  /usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib64/libgcc_s.so \
+  /usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib64/libgcc_s.so \
+  /usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib64/libgcc_s.so \
+  /usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib64/libgcc_s.so.1 \
+  /usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/14.2.1/libgcc.a \
+  /usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib64/libc.so \
+  /usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib64/libc.so \
+  /usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib64/libc.so \
+  /usr/lib/libc.so.6 \
+  /usr/lib/libc_nonshared.a \
+  /usr/lib/ld-linux-x86-64.so.2 \
+  /usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/14.2.1/libgcc.a \
+  /usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib64/libgcc_s.so \
+  /usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib64/libgcc_s.so \
+  /usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib64/libgcc_s.so \
+  /usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib64/libgcc_s.so.1 \
+  /usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/14.2.1/libgcc.a \
+  /usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/14.2.1/crtendS.o \
+  /usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib64/crtn.o
+
+/usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib64/crti.o:
+
+/usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/14.2.1/crtbeginS.o:
+
+CMakeFiles/glfw.dir/context.c.o:
+
+CMakeFiles/glfw.dir/init.c.o:
+
+CMakeFiles/glfw.dir/input.c.o:
+
+CMakeFiles/glfw.dir/monitor.c.o:
+
+CMakeFiles/glfw.dir/platform.c.o:
+
+CMakeFiles/glfw.dir/vulkan.c.o:
+
+CMakeFiles/glfw.dir/window.c.o:
+
+CMakeFiles/glfw.dir/egl_context.c.o:
+
+CMakeFiles/glfw.dir/osmesa_context.c.o:
+
+CMakeFiles/glfw.dir/null_init.c.o:
+
+CMakeFiles/glfw.dir/null_monitor.c.o:
+
+CMakeFiles/glfw.dir/null_window.c.o:
+
+CMakeFiles/glfw.dir/null_joystick.c.o:
+
+CMakeFiles/glfw.dir/posix_module.c.o:
+
+CMakeFiles/glfw.dir/posix_time.c.o:
+
+CMakeFiles/glfw.dir/posix_thread.c.o:
+
+CMakeFiles/glfw.dir/x11_init.c.o:
+
+CMakeFiles/glfw.dir/x11_monitor.c.o:
+
+CMakeFiles/glfw.dir/x11_window.c.o:
+
+CMakeFiles/glfw.dir/xkb_unicode.c.o:
+
+CMakeFiles/glfw.dir/glx_context.c.o:
+
+CMakeFiles/glfw.dir/wl_init.c.o:
+
+CMakeFiles/glfw.dir/wl_monitor.c.o:
+
+CMakeFiles/glfw.dir/wl_window.c.o:
+
+CMakeFiles/glfw.dir/linux_joystick.c.o:
+
+CMakeFiles/glfw.dir/posix_poll.c.o:
+
+/usr/lib/librt.a:
+
+/usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib64/libm.so:
+
+/usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib64/libm.so:
+
+/usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib64/libm.so:
+
+/usr/lib/libm.so.6:
+
+/usr/lib/libmvec.so.1:
+
+/usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib64/libdl.a:
+
+/usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/14.2.1/libgcc.a:
+
+/usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib64/libgcc_s.so:
+
+/usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib64/libgcc_s.so:
+
+/usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib64/libgcc_s.so:
+
+/usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib64/libgcc_s.so.1:
+
+/usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/14.2.1/libgcc.a:
+
+/usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib64/libc.so:
+
+/usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib64/libc.so:
+
+/usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib64/libc.so:
+
+/usr/lib/libc.so.6:
+
+/usr/lib/libc_nonshared.a:
+
+/usr/lib/ld-linux-x86-64.so.2:
+
+/usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/14.2.1/libgcc.a:
+
+/usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib64/libgcc_s.so:
+
+/usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib64/libgcc_s.so:
+
+/usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib64/libgcc_s.so:
+
+/usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib64/libgcc_s.so.1:
+
+/usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/14.2.1/libgcc.a:
+
+/usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/14.2.1/crtendS.o:
+
+/usr/bin/../lib64/gcc/x86_64-pc-linux-gnu/14.2.1/../../../../lib64/crtn.o:
