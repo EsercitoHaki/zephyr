1c284fc7c8349c2c77aa7dbbcb37aa59adb38c5c
EsercitoHaki <esercitohaki2003@gmail.com>
2025-04-18 21:07:08 +0700

    wip: add compute shader for vertex skinning with buffer reference

--- engine/src/DrawCommand.h
+++ engine/src/DrawCommand.h
@@ -3,11 +3,13 @@
 #include <Math/Sphere.h>
 #include <glm/mat4x4.hpp>
 
-struct GPUMesh;
+#include <Math/Sphere.h>
+
+#include <Graphics/IdTypes.h>
 
 struct DrawCommand {
-    const GPUMesh& mesh;
-    std::size_t meshId;
+    MeshId meshId;
+
     glm::mat4 transformMatrix;
     math::Sphere worldBoundingSphere;
 };
--- engine/src/Graphics/Material.h
+++ engine/src/Graphics/Material.h
@@ -1,32 +1,25 @@
 #pragma once
 
-#include <limits>
 #include <string>
 
 #include <glm/vec4.hpp>
-
 #include <VkTypes.h>
-
 #include <vulkan/vulkan.h>
-
 struct MaterialData {
     glm::vec4 baseColor;
     glm::vec4 metalRoughnessFactors;
     glm::vec4 _padding[14];
 };
 
-using MaterialId = std::size_t;
-static const auto NULL_MATERIAL_ID = std::numeric_limits<std::size_t>::max();
-
 struct Material {
-    std::string name;
-
-    AllocatedImage diffuseTexture;
-    VkDescriptorSet materialSet;
-
     glm::vec4 baseColor{1.f, 1.f, 1.f, 1.f};
     float metallicFactor{0.f};
     float roughnessFactor{0.5f};
 
     bool hasDiffuseTexture{false};
+
+    AllocatedImage diffuseTexture;
+    VkDescriptorSet materialSet;
+
+    std::string name;
 };
